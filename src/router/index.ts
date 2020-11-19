10d3c7ac1ae6622bf2780a250eb7660bb62f030e
João Apostulo <japostulo@gmail.com>
2020-11-19 19:55:33 -0300

    Estrutura inicial do projeto

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    redirect: 'Login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/*webpackChunkName: 'Login'*/ '../views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+        <div>
+            <label class="bg-blue-200" for="">Login 
+                <input type="text" class="">
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
