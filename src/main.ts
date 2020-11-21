0aac212d8a74f1cced74e75e4f0752c02e2a04b6
João Apostulo <japostulo@gmail.com>
2020-11-20 21:49:36 -0300

    Commit inicial

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/tailwind.css'
+import axios from 'axios'
+import dotenv from 'dotenv'
 
+dotenv.config();
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL;
+
+if(localStorage.token != null){
+  axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.token}`;
+}
+
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
